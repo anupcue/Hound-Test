06bc56c24afdae293a443d311b687f903cab4267
Anup Mohan <RBHW37@motorolasolutions.com>
2017-08-02 15:43:57 -0500

    Added print to 100

--- file1.py
+++ file1.py
@@ -3,6 +3,10 @@ def print1to10():
     for i in xrange(1,11):
         print i
 
+def print1to100():
+    for i in xrange(1,101):
+        print i
 
 if __name__ == "__main__":
     print1to10()
+    print1to100()
